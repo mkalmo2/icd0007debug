a57bb207b075e26f5fb7c7f071d7fa9a97e353e1
Märt Kalmo <mart.kalmo@gmail.com>
2021-03-11 13:30:30 +0200

    added input demo file

---
+++ input.php
@@ -0,0 +1,22 @@
+<?php
+
+var_dump($_POST);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+
+    <form method="post">
+        <input />
+
+        <button>Button</button>
+    </form>
+
+</body>
+</html>
