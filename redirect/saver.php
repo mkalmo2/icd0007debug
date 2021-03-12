e2bd3f1a13f3ebb9d6f4dd4b9c9a68720900fbfa
Märt Kalmo <mart.kalmo@gmail.com>
2021-03-12 09:47:56 +0200

    redirect solution removed

--- redirect/saver.php
+++ redirect/saver.php
@@ -1,13 +1,7 @@
 <?php
 
-error_log(print_r($_POST, true));
-
 $data = $_POST['data'] ?? '';
 
-error_log('save: ' . $data);
-
-$url = 'index.php?message=' . "Data\n saved!";
-
-error_log('url: ' . $url);
+$url = 'index.php?message=' . "Data saved!";
 
 header('Location: ' . $url);
