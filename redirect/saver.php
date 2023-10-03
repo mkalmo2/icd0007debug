fb6d115505582d2ae3051c3762aa8c9a946072ed
Märt Kalmo <mart.kalmo@gmail.com>
2023-10-03 09:51:57 +0300

    redirect ex fix

--- redirect/saver.php
+++ redirect/saver.php
@@ -4,7 +4,7 @@ $data = $_POST['data'] ?? '';
 
 $message = "Data saved!";
 
-if (isset($_POST['save'])) {
+if (isset($_POST['save']) && $_POST['save']) {
     file_put_contents('data.txt', $data);
     $message = sprintf("\nSaved %s bytes\n", strlen($data));
 }
