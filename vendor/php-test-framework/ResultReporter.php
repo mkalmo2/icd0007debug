b4b76147a89b73998d54223bf32e456ced622ad6
Märt Kalmo <mart.kalmo@gmail.com>
2024-10-03 11:03:04 +0300

    removed tests

--- vendor/php-test-framework/ResultReporter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace stf;
-
-interface ResultReporter {
-
-    public function execute(int $passedMethodCount): string;
-}
-
--- vendor/php-test-framework/tests/suite.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once '../public-api.php';
-
-stf\runAllTestsInDirectory(__DIR__, __FILE__);
